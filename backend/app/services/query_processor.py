02cf2dcf1c1bd45fe654023028d3170a9c42523f
ShubhPundir <shb.pndr@gmail.com>
2025-05-21 22:26:58 +0530

    query_processor refactored for 0 mathcing docs

--- backend/app/services/query_processor.py
+++ backend/app/services/query_processor.py
@@ -7,14 +7,22 @@ from app.core.llm import get_gemini_llm
 from app.core.vector_store import get_vector_store
 
 def query_documents(query: str) -> str:
-    retriever = get_vector_store().as_retriever()
-    llm = get_gemini_llm()
+    try:
+        retriever = get_vector_store().as_retriever()
+        llm = get_gemini_llm()
 
-    qa_chain = RetrievalQA.from_chain_type(
-        llm=llm,
-        retriever=retriever,
-        return_source_documents=True
-    )
-
-    result = qa_chain.invoke({"query": query})
-    return result["result"]
+        qa_chain = RetrievalQA.from_chain_type(
+            llm=llm,
+            retriever=retriever,
+            return_source_documents=True
+        )
+        result = qa_chain.invoke({"query": query})
+        
+        print("\n=== Retrieved Source Docs ===")
+        for doc in result['source_documents']:
+            print(doc.page_content[:200])  # print preview
+        print("=============================\n")
+        
+        return result["result"]
+    except Exception as e:
+        return f"an error occured: {str(e)}"
